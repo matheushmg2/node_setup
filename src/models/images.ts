1e043a904a4c3e362039a06a1ab23bcc1a55e77d
matheushmg2 <matheushmg@gmail.com>
2023-05-23 00:00:06 -0300

    adding enum and image interface

--- src/models/images.ts
+++ src/models/images.ts
@@ -1,5 +1,18 @@
 import mongoose from 'mongoose';
 
+export enum CUSTOM_IMAGE {
+  USER = 'user',
+}
+
+export interface Image {
+  _id?: string;
+  name: string;
+  size: Number;
+  key: string;
+  url: string;
+  createAt: Date;
+}
+
 const schema = new mongoose.Schema({
   name: { type: String, required: true },
   size: { type: Number },
